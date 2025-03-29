900c1e9492bae4333679705cd26ff5f19ba1a1f9
luxmiyu <luxmiyu.dev@gmail.com>
2025-03-29 04:16:17 +0100

    initial sveltekit

---
+++ src/lib/components/ProfilePicture.svelte
@@ -0,0 +1,19 @@
+<div>
+  <img src="/luxmiyu.png" alt="luxmiyu" />
+</div>
+
+<style lang="sass">
+  div
+    place-items: center
+
+    width: 100%
+    height: 100%
+
+    img
+      display: block
+
+      width: 150px
+      height: 150px
+
+      box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.2)
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  import ProfilePicture from '$lib/components/ProfilePicture.svelte'
+</script>
+
+<ProfilePicture />
