1a1775f3c462c4c15046fc5f6ee7dc9350a7410c
luxmiyu <luxmiyu.dev@gmail.com>
2025-05-10 00:59:57 +0200

    add Text component

---
+++ src/lib/components/Text.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import type { Snippet } from 'svelte'
+
+  let { children }: { children: Snippet } = $props()
+</script>
+
+<div>
+  {@render children?.()}
+</div>
+
+<style lang="sass">
+  div
+    width: 100%
+
+    display: flex
+    flex-direction: column
+    justify-content: center
+    align-items: center
+    gap: 8px
+
+    :global(p)
+      width: 100%
+      text-align: justify
+
+    :global(a)
+      text-decoration: none
+      color: var(--text-secondary)
+
+      &:hover
+        text-decoration: underline
+</style>
