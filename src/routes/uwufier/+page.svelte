dc8732cc3a5276a44d11ba0193acfae6e4676291
luxmiyu <luxmiyu.dev@gmail.com>
2025-05-10 03:51:50 +0200

    add copy and reset buttons

--- src/routes/uwufier/+page.svelte
+++ src/routes/uwufier/+page.svelte
@@ -1,9 +1,18 @@
 <script lang="ts">
-  import { Header, Footer, Container, Text, Input, Output } from '$lib/components'
+  import { Header, Footer, Container, Text, Input, Output, Grid, Button } from '$lib/components'
   import { uwufy } from './uwu'
 
   let input = $state('')
   let output = $derived(uwufy(input))
+
+  function copy() {
+    navigator.clipboard.writeText(output)
+    alert('Copied to clipboard')
+  }
+
+  function reset() {
+    input = ''
+  }
 </script>
 
 <Container fill>
@@ -15,6 +24,10 @@
     <Input.Textarea bind:value={input} width="100%" vertical />
     <p class="subtitle">an uwu wiww git da twanswated tex in dis otha box</p>
     <Output bind:value={output} width="100%" />
+    <Grid columns="2">
+      <Button onclick={copy} disabled={!output}>copi tex</Button>
+      <Button onclick={reset}>delet tex</Button>
+    </Grid>
   </Text>
 
   <Footer />
