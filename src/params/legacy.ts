5ecc2dc793c8f207044e248777886570af16dd05
luxmiyu <luxmiyu.dev@gmail.com>
2025-05-10 15:28:34 +0200

    update luxmiyu.com/progress

--- src/params/legacy.ts
+++ src/params/legacy.ts
@@ -4,7 +4,6 @@ export const legacy = [
   'convert',
   'color',
   'rng',
-  'progress',
   'base64',
   'base',
   'binary',
---
+++ src/routes/progress/+page.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+  import { browser } from '$app/environment'
+  import { Header, Footer, Container, Text } from '$lib/components'
+
+  let now = $state(new Date())
+  let percentage = $derived.by(() => {
+    const start = new Date(now.getFullYear(), 0, 1)
+    const end = new Date(now.getFullYear() + 1, 0, 1)
+    return ((now.getTime() - start.getTime()) / (end.getTime() - start.getTime())) * 100
+  })
+
+  if (browser) {
+    setInterval(() => {
+      now = new Date()
+    }, 10)
+  }
+</script>
+
+<Container fill>
+  <Header title="progress" description="a progress bar for the current year" />
+
+  <div class="container">
+    <div class="bar"></div>
+  </div>
+
+  <Text>
+    <p class="subtitle center">{now.getFullYear()} is currently {percentage.toFixed(7)}% done</p>
+  </Text>
+
+  <Footer />
+</Container>
+
+<style lang="sass">
+  .container
+    width: 100%
+    height: 36px
+
+    border: 2px solid var(--text)
+    padding: 2px
+
+    .bar
+      width: 30%
+      height: 100%
+      background: var(--text)
+
+      background: linear-gradient(-45deg, var(--red), var(--yellow), var(--green), var(--blue))
+      animation: gradient 15s ease infinite
+      background-size: 400% 400%
+
+      @keyframes gradient
+        0%
+          background-position: 0% 50%
+        
+        50%
+          background-position: 100% 50%
+        
+        100%
+          background-position: 0% 50%
+</style>
