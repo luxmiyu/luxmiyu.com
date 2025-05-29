b51e1ceba8a9019ebf36a0ba854018c27cfa6749
luxmiyu <luxmiyu.dev@gmail.com>
2025-05-29 17:50:16 +0200

    lowercase comments

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,7 +10,7 @@
 </script>
 
 <svelte:head>
-  <!-- Google Fonts -->
+  <!-- google fonts -->
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="" />
   <link
