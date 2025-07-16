e7e613301e61449124c96b006005bc92f66aa1ea
luxmiyu <luxmiyu.dev@gmail.com>
2025-07-16 14:34:08 +0200

    replace deprecated import "as: 'raw'"

--- src/routes/blog/[md=md]/+server.ts
+++ src/routes/blog/[md=md]/+server.ts
@@ -3,8 +3,12 @@ import { error } from '@sveltejs/kit'
 export async function GET({ params }) {
   const slug = params.md.replace('.md', '')
 
-  const files = import.meta.glob('../posts/*.md', { as: 'raw' })
+  const files = import.meta.glob('../posts/*.md', { query: '?raw', import: 'default' })
   const content = await files[`../posts/${slug}.md`]()
+
+  if (typeof content !== 'string') {
+    throw error(404, `Could not find ${slug}`)
+  }
 
   try {
     return new Response(content, {
