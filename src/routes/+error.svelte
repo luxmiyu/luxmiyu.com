abb0cedf02d01cdb50b2a3f03b09b167bfb54b3c
luxmiyu <luxmiyu.dev@gmail.com>
2025-07-16 15:13:06 +0200

    add +error.svelte

---
+++ src/routes/+error.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import { page } from '$app/state'
+  import { Head, Header, Footer, Container, Text, Grid, Box } from '$lib/components'
+
+  let data = $derived({
+    status: page.status.toString(),
+    error: page.error?.message ?? 'Unknown Error',
+  })
+</script>
+
+<Head title="luxmiyu → empty" description="empty" image="/preview/empty.jpg" />
+
+<Container fill>
+  <Header title={data.status} description={data.error} />
+
+  <Grid>
+    <Box>
+      <Text>
+        <p>You've found error code <code>{data.status}</code> with this message:</p>
+        <blockquote>
+          <p>{data.error}</p>
+        </blockquote>
+      </Text>
+    </Box>
+  </Grid>
+
+  <Footer />
+</Container>
